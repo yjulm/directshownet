d850eade563a28734071ed3100e2cea3e0ec3af1
nobody <nobody@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-06-30 22:31:58 +0000

    This commit was manufactured by cvs2svn to create tag 'V1-0_Beta1'.

---
+++ tags/V1-0_Beta1/directshowlib/src/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+#region license
+
+/*
+DirectShowLib - Provide access to DirectShow interfaces via .NET
+Copyright (C) 2005
+http://sourceforge.net/projects/directshownet/
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly : AssemblyTitle("Direct Show Net Library")]
+[assembly : AssemblyDescription(".NET Interfaces for calling DirectShow.  See http://directshownet.sourceforge.net/")]
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("")]
+[assembly : AssemblyProduct("BETA VERSION")]
+[assembly : AssemblyCopyright("The Apache Software License, Version 1.1")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
+[assembly : AssemblyVersion("0.9.0.0")]
+[assembly : AssemblyDelaySign(false)]
+// Path is relative to the resulting executable (\Bin\Debug)
+[assembly : AssemblyKeyFile("..\\..\\DShowNET.snk")]
+[assembly : AssemblyKeyName("")]
+[assembly : ComVisible(false)]
+[assembly : CLSCompliant(true)]
+[assembly : SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode=true)]

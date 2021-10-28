11fe1347483675e165e8f22671fa42881816d27e
Yjulm <yjulm@sina.cn>
2021-10-28 14:48:50 +0800

    vs2013 support

--- directshowlib/src/AssemblyInfo.cs
+++ directshowlib/src/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Security.Permissions;
 [assembly : AssemblyKeyName("")]
 [assembly : ComVisible(false)]
 [assembly : CLSCompliant(true)]
-[assembly : SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode=true)]
+//[assembly : SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode=true)]
